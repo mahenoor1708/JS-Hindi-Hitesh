28979c4fe0541d0a880f371b83945574399335e5
mahenoor1708 <140686035+mahenoor1708@users.noreply.github.com>
2025-03-08 16:22:15 +0000

    datatypes done

--- js/02_datatypes.js
+++ js/02_datatypes.js
@@ -8,3 +8,26 @@
 
 /* Code Readability should be high */
 
+let name="hitesh"
+let age=22
+let isLoggedIn = false
+
+/* Primitive Data Types */
+
+// number => 2 to power 53
+// string = ""
+// bigint
+// boolean = true/false
+// null => stanalone value
+// undefined => value is not assigned yet
+// symbol => for uniqueness (is used in react for identifying components)
+
+// object
+
+// console.log(typeof "mahenoor"); 
+
+console.log(typeof name); 
+console.log(typeof age); 
+console.log(typeof null); /* => object (its type is an object) */
+console.log(typeof undefined); /* type is undefined */
+
