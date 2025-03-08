9e9d55efa85ef760c930da55d8d09570afc21360
mahenoor1708 <140686035+mahenoor1708@users.noreply.github.com>
2025-03-08 12:29:12 +0000

    variables and js part

--- js/variable.js
+++ js/variable.js
@@ -1,1 +1,34 @@
-console.log("mahenoor")
+// console.log("mahenoor")
+
+// Constants
+
+const accountId = 14453
+// Value cant be changed further (Not allowed)
+// accountId=2
+
+let accountEmail = "abc@gmail.com"  // Reserves a space in memory and can be changed
+var accountPaasword = "12345" 
+accountCity ="Jaipur"
+let accountState; /* Gives output as undefined */
+// Reserves a space in memory and can be changed 
+// (Is not used because of scope) i.e block scope and functional scope as var does not has block scope
+
+// accountEmail ="xyz@gmail.com"
+// accountPaasword ="abc123"
+// accountCity="Maharashtra"
+
+// if condition{
+
+
+// } /* Scope */ 
+
+
+
+console.log([accountEmail,accountCity,accountPaasword,accountId,accountState]);
+
+
+
+
+
+
+
