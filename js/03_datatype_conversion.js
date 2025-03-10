3d7b7503731bd032e06c38ac5dff3a81a60bbb75
mahenoor1708 <140686035+mahenoor1708@users.noreply.github.com>
2025-03-10 03:11:35 +0000

    datatypes updated

--- js/03_datatype_conversion.js
+++ js/03_datatype_conversion.js
@@ -1,10 +1,10 @@
-let age = 33
+// let age = 33
 
 /*
 console.log(typeof age); => number
 */
 
-let age = "33"
+// let age = "33"
 
 /*
 console.log(typeof age); => string
@@ -12,5 +12,24 @@ console.log(typeof age); => string
 
 // Conversion of number into string
 
+// let age = "33"
+// let valueInNumber = Number(age)
+// console.log(typeof valueInNumber);
 
+/*
+converts the above string to number
+*/
 
+// string in numbers with characters
+
+// let age = "33abc"
+// let valueInNumber = Number(age)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); /* => output type NaN (NaN is a special type) */
+
+// Using Null Value
+
+let age = null
+let valueInNumber = Number(age)
+console.log(typeof valueInNumber);
+console.log(valueInNumber); /* Gives output as 0 (this case occurs sometimes when it is not able to convert properly) */
